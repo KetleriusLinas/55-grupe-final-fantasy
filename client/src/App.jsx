63de6de86814cee1c1daf602ec89ff794e2d0de8
Linas <ketleriuslinas@gmail.com>
2025-08-03 14:37:43 +0300

    server part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { HomePage } from './pages/public/Home';
-import { PublicLayout } from './templates/PublicLayout';
+import { HomePage } from './pages/public/Home.jsx';
+import { PublicLayout } from './templates/PublicLayout.jsx';
 
 export function App() {
   return (
