8bed9496da7615b3d93639446b5797cb6c0da3ab
Linas <ketleriuslinas@gmail.com>
2025-08-04 20:22:51 +0300

    Categories page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { MoviesPage } from './pages/public/Movies.jsx';
 import { CategoriesPage } from "./pages/public/Categories";
 import { LoginPage } from "./pages/public/Login";
 import { RegisterPage } from "./pages/public/Register.jsx";
-import { NotFound } from './pages/public/NotFound.jsx';
+import { NotFoundPage } from './pages/public/NotFound.jsx';
 
 export function App() {
   return (
@@ -13,11 +13,11 @@ export function App() {
       <Routes>
         <Route element={<PublicLayout />} >
           <Route path='/' index element={<HomePage />} />
-          <Route path='*' index element={<NotFound />} />
-          <Route path='/movies' index element={<MoviesPage />} />
-          <Route path='/categories' index element={<CategoriesPage />} />
-          <Route path='/login' index element={<LoginPage />} />
-          <Route path='/register' index element={<RegisterPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+          <Route path='/movies' element={<MoviesPage />} />
+          <Route path='/categories' element={<CategoriesPage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/public/Categories.jsx
+++ client/src/pages/public/Categories.jsx
@@ -1,5 +1,49 @@
+import { Link } from "react-router";
+
 export function CategoriesPage() {
+    const categoriesData = [
+        {
+            title: 'Action',
+            description: 'Lorem ipsum dolor sit amet.',
+            moviesCount: 7,
+            urlSlug: 'action',
+        },
+        {
+            title: 'Crime',
+            description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ame.',
+            moviesCount: 14,
+            urlSlug: 'crime',
+        },
+        {
+            title: 'Sci-Fi',
+            description: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit ame. Lorem ipsum dolor sit ame',
+            moviesCount: 71,
+            urlSlug: 'sci-fi',
+        },
+    ];
+
     return (
-        <h1>CATEGORIES PAGE CONTENT</h1>
+        <main>
+            <div className="container">
+                <div className="row">
+                    <h1 className="col-12 display-1 mb-5">Categories</h1>
+                </div>
+            </div>
+
+            <div className="container px-4 py-5" id="featured-3">
+                <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    {
+                        categoriesData.map(category => (
+                            <div className="feature col my-4">
+                                <h3 className="fs-2 text-body-emphasis">{category.title}</h3>
+                                <p>{category.description}</p>
+                                <p>{category.moviesCount}</p>
+                                <Link to={'/categories' + category.urlSlug} className="icon-link">Read more</Link>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </main>
     );
 }
