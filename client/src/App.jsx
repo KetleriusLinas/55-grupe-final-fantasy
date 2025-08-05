6ab010b87881f8180b428464c49519494a8e7095
Linas <ketleriuslinas@gmail.com>
2025-08-05 18:38:13 +0300

    movies/ categories pages + routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,19 @@ import { CategoryInnerPage } from './pages/public/CategoryInner.jsx';
 import { MovieInnerPage } from './pages/public/MovieInner.jsx';
 import { AdminDashboardPage } from './pages/admin/Dashboard.jsx';
 import { AdminLayout } from './templates/AdminLayout.jsx';
+import { AdminCategoriesAllPage } from './pages/admin/categories/CategoriesAll.jsx';
+import { AdminNewCategoryPage } from './pages/admin/categories/NewCategory.jsx';
+import { AdminViewCategoryPage } from './pages/admin/categories/ViewCategory.jsx';
+import { AdminEditCategoryPage } from './pages/admin/categories/EditCategory.jsx';
+import { AdminCategoriesPublishedPage } from './pages/admin/categories/CategoriesPublished.jsx';
+import { AdminCategoriesDraftPage } from './pages/admin/categories/CategoriesDraft.jsx';
+import { AdminMoviesAllPage } from './pages/admin/movies/MoviesAll';
+import { AdminNewMoviePage } from './pages/admin/movies/NewMovie';
+import { AdminViewMoviePage } from './pages/admin/movies/ViewMovie';
+import { AdminEditMoviePage } from './pages/admin/movies/EditMovie';
+import { AdminMoviesPublishedPage } from './pages/admin/movies/MoviesPublished';
+import { AdminMoviesDraftPage } from './pages/admin/movies/MoviesDraft';
+
 
 export function App() {
   return (
@@ -27,6 +40,22 @@ export function App() {
 
         <Route element={<AdminLayout />} >
           <Route path='/admin' element={<AdminDashboardPage />} />
+
+          <Route path='/admin/movies' element={<AdminMoviesAllPage />} />
+          <Route path='/admin/movies/new' element={<AdminNewMoviePage />} />
+          <Route path='/admin/movies/:movie' element={<AdminViewMoviePage />} />
+          <Route path='/admin/movies/:movie/edit' element={<AdminEditMoviePage />} />
+          <Route path='/admin/movies/published' element={<AdminMoviesPublishedPage />} />
+          <Route path='/admin/movies/draft' element={<AdminMoviesDraftPage />} />
+
+          <Route path='/admin/categories' element={<AdminCategoriesAllPage />} />
+          <Route path='/admin/categories/new' element={<AdminNewCategoryPage />} />
+          <Route path='/admin/categories/:category' element={<AdminViewCategoryPage />} />
+          <Route path='/admin/categories/:category/edit' element={<AdminEditCategoryPage />} />
+          <Route path='/admin/categories/published' element={<AdminCategoriesPublishedPage />} />
+          <Route path='/admin/categories/draft' element={<AdminCategoriesDraftPage />} />
+
+
         </Route>
 
         <Route element={<PublicLayout />} >
