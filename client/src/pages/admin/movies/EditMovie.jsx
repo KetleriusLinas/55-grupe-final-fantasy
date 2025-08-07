87c24d3cadc612124a4bbfb03fc166842a32cd08
Linas <ketleriuslinas@gmail.com>
2025-08-07 09:44:27 +0300

    AdminMovieForm finished

--- client/src/pages/admin/movies/EditMovie.jsx
+++ client/src/pages/admin/movies/EditMovie.jsx
@@ -3,6 +3,19 @@ import { AdminMovieForm } from "../../../components/forms/AdminMovieForm";
 
 
 export function AdminEditMoviePage() {
+
+    // const movie = {
+    //     img: '/vite.svg',
+    //     title: 'Action',
+    //     url: 'action',
+    //     description: 'Very action, much movie',
+    //     duration: 122,
+    //     category: 'Action',
+    //     releaseDate: '2025-06-07',
+    //     rating: 46,
+    //     status: 'published',
+    // };
+
     return (
         <main>
             <AdminPageTitle title="Edit movie" />
