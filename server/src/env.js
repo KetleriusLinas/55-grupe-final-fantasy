7a9d99980abd087ab29a0ceb52b048934adbae3a
Linas <ketleriuslinas@gmail.com>
2025-08-18 16:15:54 +0300

    server

---
+++ server/src/env.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+const argList = process.argv.slice(2);
+const args = {};
+
+for (const str of argList) {
+    const [key, value] = str.split('=');
+
+    if (key && value && key.startsWith('--')) {
+        console.log(key, value);
+        args[key.slice(2)] = value;
+    }
+}
+
+dotenv.config({
+    path: `.env.` + args.env,
+    quiet: true, // reklamas visas 'nutildo'
+});
+
+export const NODE_ENV = process.env.NODE_ENV ?? 'dev';
+export const PORT = +process.env.PORT ?? 5517;
+export const TITLE = process.env.TITLE ?? 'Project title';
+export const DB_HOST = process.env.DB_HOST ?? 'localhost';
+export const DB_PORT = +process.env.DB_PORT ?? 3306
+export const DB_DATABASE = process.env.DB_DATABASE ?? 'test_db';
+export const DB_USER = process.env.DB_USER ?? 'test_user';
+export const DB_PASSWORD = process.env.DB_PASSWORD ?? 'test_password';
+export const COOKIE_MAX_AGE = +process.env.COOKIE_MAX_AGE ?? 3600;
+
+// const a = dotenv.config({
+//     path: `src/.env.` + args.env,
+// }).parsed;
+// export const PORT = +a.PORT ?? 5517;
+// export const TITLE = a.env.TITLE ?? 'Project title';
+// export const DB_DATABASE = a.env.DB_DATABASE ?? 'test_db';
+// export const DB_USER = a.env.DB_USER ?? 'test_user';
+// export const DB_PASSWORD = a.env.DB_PASSWORD ?? 'test_pass';
